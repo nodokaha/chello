e264903c288cdae137b19ceedbfe7c1d3f6e261a
SCP-EEE <scpeee@wail.ch>
2022-02-24 10:48:55 +0900

    add

---
+++ pkg_init.sh
@@ -0,0 +1,13 @@
+mkdir $PACKAGE_NAME
+mkdir $PACKAGE_NAME/src
+touch $PACKAGE_NAME/src/$PACKAGE_NAME.c
+cd $PACKAGE_NAME
+touch NEWS README AUTHORS ChangeLog
+printf "#include <stdio.h> \n int main(void){ printf(\"hello world\"); return 0;}" >> src/$PACKAGE_NAME.c
+echo SUBDIRS=src >> Makefile.am
+echo bin_PROGRAMS=$PACKAGE_NAME >> src/Makefile.am
+echo $PACKAGE_NAME\_SOURCES=$PACKAGE_NAME.c >> src/Makefile.am
+echo AM_CFLAGS="-O2 -g" >> src/Makefile.am
+autoscan
+sed -e "s/FULL-PACKAGE-NAME/$PACKAGE_NAME/" -e "s/VERSION/1.0/" -e "s/BUG-REPORT-ADDRESS/example@example.com/" configure.scan > configure.ac
+sed -i "5aAM_INIT_AUTOMAKE" configure.ac
